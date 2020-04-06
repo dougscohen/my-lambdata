eeac7a3ef574def5f25018b7208e245ea2476e9b
dougscohen <dougscohen@gmail.com>
2020-04-06 16:17:31 -0400

    Import function

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -7,8 +7,17 @@ def enlarge(n):
     """
     return n * 100
 
+# if in our global scope, this will mess up our ability to import other
+## functions from this file. So we need to nest it under the main
 
-# x = 5
-x = int(input("Please choose a number (like 5): "))
-result = enlarge(x)
-print(result)
+# # x = 5
+# x = int(input("Please choose a number (like 5): "))
+# result = enlarge(x)
+# print(result)
+
+if __name__ == "__main__":
+    # only invoke the code below if running this file from the command line,
+    ## not if import from another file
+    x = int(input("Please choose a number (like 5): "))
+    result = enlarge(x)
+    print(result)
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,6 +1,10 @@
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
 print("HELLO WORLD")
 
 df = pandas.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
 print(df.head())
+
+print(enlarge(9))
